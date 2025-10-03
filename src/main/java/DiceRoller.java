1b9d2ca4b7bdef379615def3665dbcc2bd359691
kamsc816 <03samuel.miller@kamsc.org>
2025-10-03 13:55:34 -0400

    done!

--- src/main/java/DiceRoller.java
+++ src/main/java/DiceRoller.java
@@ -5,9 +5,14 @@ public class DiceRoller {
         // TODO: Create Random objects for dice rolling
         // 1. Three Random objects without seeds (unseeded dice)
         // 2. One Random object with any seed value (seeded die)
-        
+        Random r = new Random();
+        Random seeded = new Random(4141);
+
         // TODO: Generate random integers for dice rolls
-        
+        System.out.println("Unseeded Die 1: " + ((r.nextInt(6)+1)));
+        System.out.println("Unseeded Die 2: " + ((r.nextInt(6)+1)));
+        System.out.println("Unseeded Die 3: " + ((r.nextInt(6)+1)));
+        System.out.print("Seeded Die: "+ ((seeded.nextInt(6)+1)));
         // TODO: Display the dice rolls - 1-6
         // Show all four dice rolls with clear labels
         
